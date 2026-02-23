c05aa3b547a22bf044c16ce39901d8c0eea414bf
MARCOSPUNCH <marcospunch@hotmail.com>
2026-02-23 11:10:12 -0300

    Changing menu

--- code/Menu.py
+++ code/Menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.ftfont import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, C_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
 
 
 class Menu:
@@ -20,11 +20,11 @@ class Menu:
 
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50,"Mountain", COLOR_ORANGE,((WIN_WIDTH / 2), 70))
-            self.menu_text(50,"Shooter", COLOR_ORANGE,((WIN_WIDTH / 2), 120))
+            self.menu_text(50,"Mountain", COLOR_WHITE,((WIN_WIDTH / 2), 70))
+            self.menu_text(50,"Shooter", COLOR_WHITE,((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], C_WHITE,((WIN_WIDTH / 2), 200 + 25 * i))
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE,((WIN_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()
             # Check for all events
